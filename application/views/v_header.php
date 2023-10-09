ddbc5fa4935fad2d2bdd9e2ddddbfdac7b700e2c
syakal239 <syahnakrihaikal@gmail.com>
2023-10-09 20:36:30 +0700

    commit

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset=”utf-8”>
+    <meta charset="utf-8">
     <title>Web Prog II | Merancang Template sederhana dengan 
 codeigniter</title>
-    <link rel='stylesheet' type=”text/css” href=”<?php echo 
-base_url() ?>assets/css/stylebuku.css'>
+    <link rel='stylesheet' type="text/css" href="<?php echo 
+base_url() ?>assets/css/stylebuku.css">
 </head>
 <body>
-    <div id=”wrapper”>
+    <div id="wrapper">
         <header>
             <hgroup>
                 <h1>RentalBuku.net</h1>
@@ -18,7 +18,7 @@ CodeIgniter</h3>
             <nav>
                 <ul>
                     <li><a href="<?php echo 
-base_url().'index.php/web' ?">Home</a></li>
+base_url().'index.php/web'?>">Home</a></li>
                     <li><a href="<?php echo 
 base_url().'index.php/web/about' ?>">About</a></li>
                 </ul>
